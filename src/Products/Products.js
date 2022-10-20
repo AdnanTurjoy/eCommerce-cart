5145576a9d097dbb5dbeca65c5910e0c0a291eab
Adnan Turjoy <aturjoy4@gmail.com>
2022-10-20 22:41:48 +0600

    update search

--- src/Products/Products.js
+++ src/Products/Products.js
@@ -4,7 +4,7 @@ import Product from "./Product";
 import ReactLoading from "react-loading";
 import { useNavigate } from "react-router-dom";
 import { cartContext } from "../App";
-
+import Search from "../components/Search/Search";
 
 function Products({ getTotalCartAddedNumber }) {
   const navigate = useNavigate();
@@ -12,22 +12,9 @@ function Products({ getTotalCartAddedNumber }) {
 
   const [loading, setLoading] = useState(false);
   const [products, setProducts] = useState([]);
+  const [SearchedProduct,setSearchedProduct] = useState([]);
   const [cart, setCart] = useState([]);
-  // const [Product,setProduct] = useState({})
-  // getting current user uid
-  // function GetUserUid() {
-  //   const [uid, setUid] = useState(null);
-  //   useEffect(() => {
-  //     auth.onAuthStateChanged((user) => {
-  //       if (user) {
-  //         setUid(user.uid);
-  //       }
-  //     });
-  //   }, []);
-  //   return uid;
-  // }
 
-  // const uid = GetUserUid();
 
   // getting products function
   const getProducts = async () => {
@@ -43,6 +30,7 @@ function Products({ getTotalCartAddedNumber }) {
         if (productsArray.length === products.docs.length) {
           setLoading(false);
           setProducts(productsArray);
+          setSearchedProduct(productsArray);
         }
       }
     } else {
@@ -51,12 +39,18 @@ function Products({ getTotalCartAddedNumber }) {
   };
 
   useEffect(() => {
-    
     setLoading(true);
     getProducts();
-   
   }, []);
 
+  const handleSearch = (searchText) => {
+    let value = searchText.toLowerCase();
+    const SearchedProductText = products.filter((product) => {
+      const productValue = product.title.toLowerCase();
+      return productValue.includes(value);
+    });
+    setSearchedProduct(SearchedProductText);
+  };
   useEffect(() => {
     getTotalCartAddedNumber(cart.length);
     getSelectedCart(cart);
@@ -67,19 +61,22 @@ function Products({ getTotalCartAddedNumber }) {
   };
   //console.log(cart);
   return (
-    <div className="m-auto content-center">
-      {loading && (
-        <ReactLoading
-          className="m-auto content-center"
-          type="bubbles"
-          color="#122c40"
-          height={75}
-          width={100}
-        />
-      )}
-      
-      <Product products={products} addToCart={addToCart} />
-    </div>
+    <>
+      <Search handleSearch={handleSearch} />
+      <div className="m-auto content-center">
+        {loading && (
+          <ReactLoading
+            className="m-auto content-center"
+            type="bubbles"
+            color="#122c40"
+            height={75}
+            width={100}
+          />
+        )}
+
+        <Product products={SearchedProduct} addToCart={addToCart} />
+      </div>
+    </>
   );
 }
 
